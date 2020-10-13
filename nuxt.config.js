6e6795a0e78ece42b41aa244a407f8e088afc23f
AgustinOsses <ossesagus@gmail.com>
2020-10-13 12:07:02 -0300

    Changed to variable single quotes in nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,13 +12,13 @@ module.exports = {
     modules: [
         'nuxt-i18n', ['@nuxtjs/firebase', {
             config: {
-                apiKey: process.env[`VUE_APP_FIREBASE_API_KEY`],
-                authDomain: process.env[`VUE_APP_FIREBASE_AUTH_DOMAIN`],
-                databaseURL: process.env[`VUE_APP_FIREBASE_URL_DB`],
-                projectId: process.env[`VUE_APP_FIREBASE_PROJECT_ID`],
-                storageBucket: process.env[`VUE_APP_FIREBASE_STORAGE_BUCKET`],
-                messagingSenderId: process.env[`VUE_APP_FIREBASE_MESSAGEINGSENDERID`],
-                appId: process.env[`VUE_APP_FIREBASE_APP_ID`]
+                apiKey: process.env['VUE_APP_FIREBASE_API_KEY'],
+                authDomain: process.env['VUE_APP_FIREBASE_AUTH_DOMAIN'],
+                databaseURL: process.env['VUE_APP_FIREBASE_URL_DB'],
+                projectId: process.env['VUE_APP_FIREBASE_PROJECT_ID'],
+                storageBucket: process.env['VUE_APP_FIREBASE_STORAGE_BUCKET'],
+                messagingSenderId: process.env['VUE_APP_FIREBASE_MESSAGEINGSENDERID'],
+                appId: process.env['VUE_APP_FIREBASE_APP_ID']
             },
             services: {
                 auth: {
